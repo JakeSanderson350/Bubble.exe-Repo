7b8848d13010648399920587a4c14ddcc9772ff2
JakeSanderson350 <jake.sanderson@mymail.champlain.edu>
2025-01-25 17:02:47 -0500

    dOWNLOAD FILE TASK done

--- Assets/Scripts/DownloadFileTask.cs
+++ Assets/Scripts/DownloadFileTask.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class DownloadFileTask : MonoBehaviour
 {
@@ -9,7 +10,17 @@ public class DownloadFileTask : MonoBehaviour
     [SerializeField]
     GameObject loadingBar;
 
+    [SerializeField]
+    GameObject ParentObj;
+
     private bool isDownloading = false;
+    private bool isDownloaded = false;
+
+    // Loading Bar variables
+    [SerializeField] Image[] loadPoints;
+
+    private float loadProgress = 0;
+    private float loadSpeedMultiplier = 0f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -22,15 +33,76 @@ public class DownloadFileTask : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //Add to load progress
+        if (isDownloading)
+        {
+            loadProgress += (loadSpeedMultiplier * Time.deltaTime);
+        }
+
+        if (loadProgress < 25)
+        {
+            loadSpeedMultiplier = 10.0f;
+        }
+        else if (loadProgress < 30)
+        {
+            loadSpeedMultiplier = 0.5f;
+        }
+        else if (loadProgress < 100)
+        {
+            loadSpeedMultiplier = 15.0f;
+        }
+        else if (loadProgress < 105)
+        {
+            loadSpeedMultiplier = 0.5f;
+        }
+        else if (loadProgress < 200)
+        {
+            loadSpeedMultiplier = 15.0f;
+        }
+
+        LoadingBarFiller();
+
+        if (loadProgress > 200)
+        {
+            isDownloaded = true;
+            //isDownloading = false;
+
+            // Disable loadingBar and have completed icon
+            loadingBar.SetActive(false);
+        }
+    }
+
+    private void LoadingBarFiller()
+    {
+        //enables load point images 
+        for (int i = 0; i < loadPoints.Length; i++)
+        {
+            loadPoints[i].enabled = !DisplayLoadPoint(loadProgress, i);
+        }
+    }
+
+    private bool DisplayLoadPoint(float _load, int loadPointNumber)
+    {
+        return ((loadPointNumber * 10) >= _load);
     }
 
     public void startDownload()
     {
-        if (!isDownloading)
+        if (!isDownloading && !isDownloaded)
         {
             isDownloading = true;
             loadingBar.SetActive(true);
+            loadSpeedMultiplier = 30.0f;
+
+            buttonText.text = "Close";
+        }
+    }
+
+    public void closeDownload()
+    {
+        if (isDownloaded)
+        {
+            ParentObj.SetActive(false);
         }
     }
 }
