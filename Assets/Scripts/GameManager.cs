c7d23f03e97481d23eeeb9807f35028b3f1f24ec
Joel Nyonga <joel.nyonga@mymail.champlain.edu>
2025-01-26 16:40:17 -0500

    End Screen Created

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -103,7 +103,7 @@ public class GameManager : MonoBehaviour
         }
         else
         {
-            //SceneManager.LoadScene(1);
+            SceneManager.LoadScene("GameLoseScene");
             Debug.Log("Ur asss");
         }
     }
